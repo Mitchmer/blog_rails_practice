52255f66ebeae131147a1f48ff25c1370c8f617e
Mitch Merrell <mitchmerrell88@gmail.com>
2018-02-26 12:10:01 -0700

    create pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,37 @@
+class PagesController < ApplicationController
+  def index
+    #GET /pages
+    #instance variable, so we can access all of the info on the Pages model
+    @pages = Page.all
+  end
+
+  #GET /pages/:id
+  #clicked on an individual page
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  #GET /pages/new
+  #load a new form to fill out
+  #creates a skeleton object, with all values = nil
+  def new
+    @page = Page.new
+  end
+
+  #POST /pages
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :new
+    end
+  end
+
+  
+
+  private
+    def page_params
+      params.require(:page).permit(:body, :author, :title)
+    end
+end
